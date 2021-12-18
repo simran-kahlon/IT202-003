77e535bd028a49b470cabcaeea3342f5d1b913c5
Simran Kahlon <simran@Simrans-MBP.fios-router.home>
2021-12-17 19:19:02 -0500

    my competition history

--- partials/nav.php
+++ partials/nav.php
@@ -34,6 +34,7 @@ require_once(__DIR__ . "/../lib/functions.php");
             <li><a href="<?php echo get_url('profile.php'); ?>">Profile</a></li>
             <li><a href="<?php echo get_url('create_competition.php'); ?>">Create Competition</a></li>
             <li><a href="<?php echo get_url('view_competition.php'); ?>">Active Competitions</a></li>
+            <li><a href="<?php echo get_url('my_competitions.php'); ?>">My Competitions</a></li>
         <?php endif; ?>
         <?php if (!is_logged_in()) : ?>
             <li><a href="<?php echo get_url('home.php'); ?>">Home</a></li>
